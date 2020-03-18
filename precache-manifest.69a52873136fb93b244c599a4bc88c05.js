fb020a8010926648314de6f14188c784f581ca31
GabrielNumaX <37051017+GabrielNumaX@users.noreply.github>
2020-03-18 18:30:33 -0300

    Updates

---
+++ precache-manifest.69a52873136fb93b244c599a4bc88c05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f63bdf58d63b9c9baa63c0eb110b5f3",
+    "url": "/dice-game/index.html"
+  },
+  {
+    "revision": "f9315cff8c0274b7fd33",
+    "url": "/dice-game/static/css/main.a9ce708a.chunk.css"
+  },
+  {
+    "revision": "1859495ffaa575d1c2cb",
+    "url": "/dice-game/static/js/2.b02d1ea6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/dice-game/static/js/2.b02d1ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9315cff8c0274b7fd33",
+    "url": "/dice-game/static/js/main.c8f9ab4e.chunk.js"
+  },
+  {
+    "revision": "d9f92939a1506f770052",
+    "url": "/dice-game/static/js/runtime-main.4f1ed6c8.js"
+  }
+]);
